0b4317137e98c2c61e333142b4e7281dcce2a860
Banerjee PC <souvikbanerjee@Banerjees-Mac-mini.local>
2026-05-10 22:45:38 +0530

    Mutual Fund Browser App: Basic code and dependency cleanup.

--- lib/models/nav_entry.dart
+++ lib/models/nav_entry.dart
@@ -1,3 +1,5 @@
+import 'package:mutual_fund_browser/utils/date_format.dart';
+
 class NavEntry {
   const NavEntry({required this.date, required this.nav});
 
@@ -5,16 +7,8 @@ class NavEntry {
   final double nav;
 
   factory NavEntry.fromJson(Map<String, dynamic> json) => NavEntry(
-        date: _parseDate(json['date'] as String),
+        date: parseApiDate(json['date'] as String),
         nav: double.parse(json['nav'] as String),
       );
 
-  static DateTime _parseDate(String raw) {
-    final parts = raw.split('-');
-    return DateTime(
-      int.parse(parts[2]),
-      int.parse(parts[1]),
-      int.parse(parts[0]),
-    );
-  }
 }
